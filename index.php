d891d500f670ad75889c4cd2fae33c25e99d0c86
Gilberto Diaz <gjdiaz.2020@gmail.com>
2024-03-21 00:10:43 -0500

    Se corrige ruta del css para el index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if (!isset($_SESSION['user_id'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Home</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <link rel="stylesheet" href="http://localhost/proyecto/vistas/css/index.css">
+        <link rel="stylesheet" href="vistas/css/index.css">
     </head>
     <body>
       <header>
