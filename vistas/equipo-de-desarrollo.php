58f1a37a1f1da75b3113a7eb2a944959f464a119
Kevin Salazar M <kevinsalazarmo@gmail.com>
2024-04-03 22:19:57 -0500

    ajustes en las tarjetas se elimina el parpadeo

--- vistas/equipo-de-desarrollo.php
+++ vistas/equipo-de-desarrollo.php
@@ -12,152 +12,163 @@
             <h1 class="title">Equipo de desarrollo</h1>
         </div>        
         <div class="cards-container">
-            <div class="card card-uno">
-                <div class="frontal">
-                <img src="img/imgs-equipo-de-desarrollo/kevin.jpg" alt="">
-                    <h3 class="peq">Front end</h3>
-                    <div class="imagenes">
-                        <img src="img/imgs-equipo-de-desarrollo/java.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/python.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/mysql.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/html-5.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/css-3.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/java-script.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/bootstrap.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/code.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/php.png" alt="">
+            <div class="parpadeo-uno">
+                <div class="card card-uno">
+                    <div class="frontal">
+                    <img src="img/imgs-equipo-de-desarrollo/kevin.jpg" alt="">
+                        <h3 class="peq">Front end</h3>
+                        <div class="imagenes">
+                            <img src="img/imgs-equipo-de-desarrollo/java.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/python.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/mysql.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/html-5.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/css-3.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/java-script.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/bootstrap.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/code.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/php.png" alt="">
+                        </div>
+                    </div>
+                    <div class="reverso">
+                        <p class="title-reverso carrera">Tecnologo en Analisis y Desarrollo de Software</p>
+                        <p class="title-reverso">Nombre: <span>Kevin Salazar M.</span></p>
+                        <p class="title-reverso">Desarrollador: <span>Front end</span></p>
+                        <p class="title-reverso">Edad: <span>33 años</span></p>                                        
+                        <p class="title-reverso">Tecnologias:</p>
+                        <p class="Tecnologias">
+                            <span>Java</span> 
+                            <span>Python</span> 
+                            <span>Html</span> 
+                            <span>Css</span> 
+                            <span>Botstrap5</span>
+                            <span>JavaScript</span>
+                            <span>MySQL</span>
+                            <span>GitHub</span>
+                            <span>PHP</span>
+                        <p>
+                        <p class="title-reverso correo">Correo: <span>kevinsalazarmo@gmail.com</span></p>
                     </div>
                 </div>
-                <div class="reverso">
-                    <p class="title-reverso carrera">Tecnologo en Analisis y Desarrollo de Software</p>
-                    <p class="title-reverso">Nombre: <span>Kevin Salazar M.</span></p>
-                    <p class="title-reverso">Desarrollador: <span>Front end</span></p>
-                    <p class="title-reverso">Edad: <span>33 años</span></p>                                        
-                    <p class="title-reverso">Tecnologias:</p>
-                    <p class="Tecnologias">
-                        <span>Java</span> 
-                        <span>Python</span> 
-                        <span>Html</span> 
-                        <span>Css</span> 
-                        <span>Botstrap5</span>
-                        <span>JavaScript</span>
-                        <span>MySQL</span>
-                        <span>GitHub</span>
-                        <span>PHP</span>
-                    <p>
-                    <p class="title-reverso correo">Correo: <span>kevinsalazarmo@gmail.com</span></p>
+            </div>
+
+            <div class="parpadeo-dos">
+                <div class="card card-dos">
+                    <div class="frontal">
+                        <img src="img/imgs-equipo-de-desarrollo/IMG_5725.jpg" alt="">
+                        <h3>Back end</h3>
+                        <h3 class="title-backend">Base de datos</h3>
+                        <div class="imagenes">
+                            <img src="img/imgs-equipo-de-desarrollo/java.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/python.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/mysql.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/html-5.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/css-3.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/java-script.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/bootstrap.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/code.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/php.png" alt="">
+                        </div>
+                    </div>
+                    <div class="reverso">
+                        <p class="title-reverso carrera">Tecnologo en Analisis y Desarrollo de Software</p>
+                        <p class="title-reverso">Nombre: <span>Gilberto Diaz Marquez</span></p>
+                        <p class="title-reverso enfoque">Desarrollador: <span class="rol">Back end</span><span class="rol">Base de datos</span></p>
+                        <p class="title-reverso">Edad: <span>23 años</span></p>                                        
+                        <p class="title-reverso">Tecnologias:</p>
+                        <p class="Tecnologias">
+                            <span>Java</span> 
+                            <span>Python</span> 
+                            <span>Html</span> 
+                            <span>Css</span> 
+                            <span>Botstrap5</span>
+                            <span>JavaScript</span>
+                            <span>MySQL</span>
+                            <span>GitHub</span>
+                            <span>PHP</span>
+                        <p>
+                        <p class="title-reverso correo">Correo: <span>gjdiaz.2020@gmail.com</span></p>
+                    </div>
                 </div>
-            </div>            
-            <div class="card card-dos">
-                <div class="frontal">
+            </div>
+
+            <div class="parpadeo-tres">
+                <div class="card card-tres">
+                    <div class="frontal">
+                    <img src="img/imgs-equipo-de-desarrollo/moises.jpg" alt="">
+                        <h3 class="peq">Project manager</h3>
+                        <div class="imagenes">
+                            <img src="img/imgs-equipo-de-desarrollo/java.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/python.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/mysql.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/html-5.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/css-3.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/java-script.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/bootstrap.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/code.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/php.png" alt="">
+                        </div>
+                    </div>
+                    <div class="reverso">
+                        <p class="title-reverso carrera">Tecnologo en Analisis y Desarrollo de Software</p>
+                        <p class="title-reverso">Nombre: <span>Moises Martinez</span></p>
+                        <p class="title-reverso">Desarrollador: <span>Bases de datos</span></p>
+                        <p class="title-reverso">Edad: <span>24 años</span></p>                                        
+                        <p class="title-reverso">Tecnologias:</p>
+                        <p class="Tecnologias">
+                            <span>Java</span> 
+                            <span>Python</span> 
+                            <span>Html</span> 
+                            <span>Css</span> 
+                            <span>Botstrap5</span>
+                            <span>JavaScript</span>
+                            <span>MySQL</span>
+                            <span>GitHub</span>
+                            <span>PHP</span>
+                        <p>
+                        <p class="title-reverso correo">Correo: <span>mmartinez1310@hotmail.com</span></p>
+                    </div>
+                </div>
+            </div>                        
+                        
+            <div class="parpadeo-cuatro">
+                <div class="card card-cuatro">
+                    <div class="frontal">
                     <img src="img/imgs-equipo-de-desarrollo/IMG_5725.jpg" alt="">
-                    <h3>Back end</h3>
-                    <h3 class="title-backend">Base de datos</h3>
-                    <div class="imagenes">
-                        <img src="img/imgs-equipo-de-desarrollo/java.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/python.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/mysql.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/html-5.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/css-3.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/java-script.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/bootstrap.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/code.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/php.png" alt="">
+                        <h3>Tester</h3>
+                        <h3 class="title-backend">Diseño UX/UI</h3>
+                        <div class="imagenes">
+                            <img src="img/imgs-equipo-de-desarrollo/java.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/python.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/mysql.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/html-5.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/css-3.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/java-script.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/bootstrap.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/code.png" alt="">
+                            <img src="img/imgs-equipo-de-desarrollo/php.png" alt="">
+                        </div>
                     </div>
-                </div>
-                <div class="reverso">
-                    <p class="title-reverso carrera">Tecnologo en Analisis y Desarrollo de Software</p>
-                    <p class="title-reverso">Nombre: <span>Gilberto Diaz Marquez</span></p>
-                    <p class="title-reverso enfoque">Desarrollador: <span class="rol">Back end</span><span class="rol">Base de datos</span></p>
-                    <p class="title-reverso">Edad: <span>23 años</span></p>                                        
-                    <p class="title-reverso">Tecnologias:</p>
-                    <p class="Tecnologias">
-                        <span>Java</span> 
-                        <span>Python</span> 
-                        <span>Html</span> 
-                        <span>Css</span> 
-                        <span>Botstrap5</span>
-                        <span>JavaScript</span>
-                        <span>MySQL</span>
-                        <span>GitHub</span>
-                        <span>PHP</span>
-                    <p>
-                    <p class="title-reverso correo">Correo: <span>gjdiaz.2020@gmail.com</span></p>
+                    <div class="reverso">
+                        <p class="title-reverso carrera">Tecnologo en Analisis y Desarrollo de Software</p>
+                        <p class="title-reverso">Nombre: <span>Gilberto Diaz Marquez</span></p>
+                        <p class="title-reverso enfoque">Desarrollador: <span class="rol">Back end</span><span class="rol">Base de datos</span></p>
+                        <p class="title-reverso">Edad: <span>23 años</span></p>                                        
+                        <p class="title-reverso">Tecnologias:</p>
+                        <p class="Tecnologias">
+                            <span>Java</span> 
+                            <span>Python</span> 
+                            <span>Html</span> 
+                            <span>Css</span> 
+                            <span>Botstrap5</span>
+                            <span>JavaScript</span>
+                            <span>MySQL</span>
+                            <span>GitHub</span>
+                            <span>PHP</span>
+                        <p>
+                        <p class="title-reverso correo">Correo: <span>gjdiaz.2020@gmail.com</span></p>
+                    </div>
                 </div>
             </div>            
-            <div class="card card-tres">
-                <div class="frontal">
-                <img src="img/imgs-equipo-de-desarrollo/moises.jpg" alt="">
-                    <h3 class="peq">Project manager</h3>
-                    <div class="imagenes">
-                        <img src="img/imgs-equipo-de-desarrollo/java.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/python.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/mysql.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/html-5.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/css-3.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/java-script.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/bootstrap.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/code.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/php.png" alt="">
-                    </div>
-                </div>
-                <div class="reverso">
-                    <p class="title-reverso carrera">Tecnologo en Analisis y Desarrollo de Software</p>
-                    <p class="title-reverso">Nombre: <span>Moises Martinez</span></p>
-                    <p class="title-reverso">Desarrollador: <span>Bases de datos</span></p>
-                    <p class="title-reverso">Edad: <span>24 años</span></p>                                        
-                    <p class="title-reverso">Tecnologias:</p>
-                    <p class="Tecnologias">
-                        <span>Java</span> 
-                        <span>Python</span> 
-                        <span>Html</span> 
-                        <span>Css</span> 
-                        <span>Botstrap5</span>
-                        <span>JavaScript</span>
-                        <span>MySQL</span>
-                        <span>GitHub</span>
-                        <span>PHP</span>
-                    <p>
-                    <p class="title-reverso correo">Correo: <span>mmartinez1310@hotmail.com</span></p>
-                </div>
-            </div>
-            <div class="card card-cuatro">
-                <div class="frontal">
-                <img src="img/imgs-equipo-de-desarrollo/IMG_5725.jpg" alt="">
-                    <h3>Tester</h3>
-                    <h3 class="title-backend">Diseño UX/UI</h3>
-                    <div class="imagenes">
-                        <img src="img/imgs-equipo-de-desarrollo/java.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/python.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/mysql.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/html-5.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/css-3.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/java-script.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/bootstrap.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/code.png" alt="">
-                        <img src="img/imgs-equipo-de-desarrollo/php.png" alt="">
-                    </div>
-                </div>
-                <div class="reverso">
-                    <p class="title-reverso carrera">Tecnologo en Analisis y Desarrollo de Software</p>
-                    <p class="title-reverso">Nombre: <span>Gilberto Diaz Marquez</span></p>
-                    <p class="title-reverso enfoque">Desarrollador: <span class="rol">Back end</span><span class="rol">Base de datos</span></p>
-                    <p class="title-reverso">Edad: <span>23 años</span></p>                                        
-                    <p class="title-reverso">Tecnologias:</p>
-                    <p class="Tecnologias">
-                        <span>Java</span> 
-                        <span>Python</span> 
-                        <span>Html</span> 
-                        <span>Css</span> 
-                        <span>Botstrap5</span>
-                        <span>JavaScript</span>
-                        <span>MySQL</span>
-                        <span>GitHub</span>
-                        <span>PHP</span>
-                    <p>
-                    <p class="title-reverso correo">Correo: <span>gjdiaz.2020@gmail.com</span></p>
-                </div>
-            </div>
         </div>           
     </body>
 </html>
